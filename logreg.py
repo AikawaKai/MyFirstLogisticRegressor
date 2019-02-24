6b7e47b20a1b79daf263c7311d78bed543254e52
Marco Odore <marco.odore@studenti.unimi.it>
2019-02-24 20:55:27 +0100

    updated

--- logreg.py
+++ logreg.py
@@ -4,7 +4,8 @@ from numpy import exp, dot, log, ones, zeros, concatenate, linspace
 from sklearn.model_selection import train_test_split
 from matplotlib.animation import FuncAnimation
 from copy import deepcopy
-from numpy import log
+from numpy import log, logspace, meshgrid, arange, full, array
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def plot_iris(X, y, type_, display=False):
@@ -38,33 +39,34 @@ class LogisticRegression(object):
         self.verbose = verbose
         self.theta = None
         self.thetas = []
+        self.losses = []
 
     # we add a vector of ones as intercept in the X matrix  (we have another weight w0 for this intercept "feature")
     @staticmethod
-    def __add_intercept(X):
+    def add_intercept(X):
         intercept = ones((X.shape[0], 1))
         return concatenate((intercept, X), axis=1)
 
     # sigmoid function
     @staticmethod
-    def __sigmoid(z):
+    def sigmoid(z):
         return 1 / (1 + exp(-z))
 
     # dot product between input and weights
     @staticmethod
-    def __get_z(X, theta):
+    def get_z(X, theta):
         return dot(X, theta)
 
     # loss function to check how well the predictor / regressor works
     # h = predictions / hypothesis
     # y = real labels
     @staticmethod
-    def __loss(h, y):
+    def loss(h, y):
         return (-y * log(h) - (1 - y) * log(1 - h)).mean()
 
     # derivative of loss function
     @staticmethod
-    def __loss_gradient(X, y, h):
+    def loss_gradient(X, y, h):
         return dot(X.T, (h - y)) / y.shape[0]
 
     # updating weights with gradient descent
@@ -74,45 +76,109 @@ class LogisticRegression(object):
 
     def fit(self, X, y):
         if self.fit_intercept:
-            X = LogisticRegression.__add_intercept(X)
+            X = LogisticRegression.add_intercept(X)
         # weights initialization
         self.theta = zeros(X.shape[1])
-        to_save = [int(z * log(z)) for z in range(1, 200)][10:]
+        self.theta = array([0.0, -5.0, 2.0])
+        to_save = [int(z * log(z)) for z in range(1, 200)]
         print(to_save)
+
         for i in range(self.num_iter):
-            z = LogisticRegression.__get_z(X, self.theta)
-            h = LogisticRegression.__sigmoid(z)
-            gradient = LogisticRegression.__loss_gradient(X, y, h)
+            z = LogisticRegression.get_z(X, self.theta)
+            h = LogisticRegression.sigmoid(z)
+            gradient = LogisticRegression.loss_gradient(X, y, h)
             self.__update_theta(gradient)
             if self.verbose and i in to_save:
                 self.thetas.append(deepcopy(self.theta))
-                z = LogisticRegression.__get_z(X, self.theta)
-                h = LogisticRegression.__sigmoid(z)
-                print(f'loss: {LogisticRegression.__loss(h, y)} \t')
-        return self.thetas, to_save
+                z = LogisticRegression.get_z(X, self.theta)
+                h = LogisticRegression.sigmoid(z)
+                loss = LogisticRegression.loss(h, y)
+                print(f'loss: {loss} \t')
+                print("theta: ", self.theta[1])
+                self.losses.append(loss)
+        return self.thetas, self.losses, to_save
 
     def predict_prob(self, X):
         if self.fit_intercept:
-            X = LogisticRegression.__add_intercept(X)
-        return LogisticRegression.__sigmoid(LogisticRegression.__get_z(X, self.theta))
+            X = LogisticRegression.add_intercept(X)
+        return LogisticRegression.sigmoid(LogisticRegression.get_z(X, self.theta))
 
     def predict(self, X, threshold=0.5):
         return self.predict_prob(X) >= threshold
 
 
-def main():
-    iris = load_iris()  # loading Dataset
-    X = iris.data[:, :2]  # two dimension out of 4
-    y = (iris.target != 0) * 1  # flattening from 3 classes to 2 classes
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.28, random_state=11)
-    #plot_iris(X_train, y_train, "Train")
-    #plot_iris(X_test, y_test, "Test")
-    lr = LogisticRegression(learning_rate=0.1, num_iterations=300000, verbose=True)
-    weights, to_save = lr.fit(X_train, y_train)
-    preds = lr.predict(X_test)
-    print((preds == y_test).mean())
-    vector_weights = lr.theta
-    print(vector_weights)
+def func_z(X, y, w1, w2, w3):
+    n1, n2 = w1.shape
+    res = full((n1, n2), None)
+    for i in range(n1):
+        for j in range(n2):
+            curr_w1 = w1[i, j]
+            curr_w2 = w2[i, j]
+            curr_w3 = w3[i, j]
+            z = LogisticRegression.get_z(X, array([curr_w1, curr_w2, curr_w3]))
+            h = LogisticRegression.sigmoid(z)
+            y_ = LogisticRegression.loss(h, y)
+            res[i, j] = y_
+    return res
+
+
+def _3d_plot(weights, X_train, y_train, losses):
+    w1_ = [weights[i][0] for i in range(len(weights))]
+    w2_ = [weights[i][1] for i in range(len(weights))]
+    w3_ = [weights[i][2] for i in range(len(weights))]
+    print(max(w2_), min(w2_))
+    print(max(w3_), min(w3_))
+    intercept = weights[-1][0]
+    w2, w3 = meshgrid(arange(-6, 5, 0.1), arange(-6, 5, 0.1))
+    lato = len(arange(-6, 5, 0.1))
+    w1 = full((lato, lato), intercept)
+    X_ = LogisticRegression.add_intercept(X_train)
+    print(X_.shape)
+    fig1 = plt.figure()
+    ax1 = Axes3D(fig1)
+    _3d = ax1.plot_surface(w2, w3, func_z(X_, y_train, w1, w2, w3), edgecolor='blue', rstride=8,
+                           cstride=5, cmap='jet')
+    ax1.plot([w2_[-1]], [w3_[-1]], losses[-1], 'r*', markersize=10)
+    line, = ax1.plot([], [], [], 'r-', label='Gradient descent', lw=1.5)
+    point, = ax1.plot([], [], [], 'bo')
+    display_value = ax1.text(2., 2., 27.5, '', transform=ax1.transAxes)
+
+    def init():
+        line.set_data([], [])
+        line.set_3d_properties([])
+        point.set_data([], [])
+        point.set_3d_properties([])
+        display_value.set_text('')
+
+        return line, point, display_value
+
+    def animate(i):
+        # Animate line
+        line.set_data(w2_[:i], w3_[:i])
+        line.set_3d_properties(losses[:i])
+
+        # Animate points
+        point.set_data(w2_[i], w3_[i])
+        point.set_3d_properties(losses[i])
+
+        # Animate display value
+        display_value.set_text('Min = ' + str(losses[i]))
+
+        return line, point, display_value
+
+    ax1.legend(loc=1)
+
+    anim = FuncAnimation(fig1, animate, init_func=init,
+                         frames=len(losses), interval=120,
+                         repeat_delay=60, blit=True, repeat=True)
+    anim.save("3d.gif", dpi=120, writer='imagemagick')
+    plt.show()
+
+
+def _2d_plot(weights, losses, to_save, X, y, filter_):
+    weights = weights[filter_:]
+    losses = losses[filter_:]
+    to_save = to_save[filter_:]
 
     fig, ax = plt.subplots()
     plt.xlabel("x1")
@@ -129,18 +195,28 @@ def main():
     line, = ax.plot(x, -((weights[0][1] / weights[0][2]) * x + weights[0][0] / weights[0][2]), 'b-', linewidth=2)
 
     def update_plot(i):
-        # print(i)
-        # print("WEIGHTS")
-        # print(-((weights[i][1] / weights[i][2]) * x + weights[i][0] / weights[i][2]))
-        print("X+{}".format(i))
-        # print(x+i)
         line.set_data(x, -((weights[i][1] / weights[i][2]) * x + weights[i][0] / weights[i][2]))
         plt.title("Iteration {}".format(to_save[i]))
         return line, ax
 
     anim = FuncAnimation(fig, update_plot, frames=range(len(weights)), interval=200)
-    anim.save("test.gif", dpi=80, writer='imagemagick')
+    anim.save("2d.gif", dpi=80, writer='imagemagick')
     plt.show()
+
+
+def main():
+    iris = load_iris()  # loading Dataset
+    X = iris.data[:, :2]  # two dimension out of 4
+    y = (iris.target != 0) * 1  # flattening from 3 classes to 2 classes
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.28, random_state=11)
+    lr = LogisticRegression(learning_rate=0.1, num_iterations=300000, verbose=True)
+    weights, losses, to_save = lr.fit(X_train, y_train)
+    preds = lr.predict(X_test)
+    print((preds == y_test).mean())
+    vector_weights = lr.theta
+    print(vector_weights)
+    _3d_plot(weights, X_train, y_train, losses)
+
     # plot_iris_with_boundaries(X, y, vector_weights, True)
 
 
